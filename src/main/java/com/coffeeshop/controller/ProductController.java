e88dcf01e23b817f9a43caf0e934572b11f0fa1f
sacha11@list.ru <https://github.com/chernulich>
2019-07-02 22:24:03 +0300

    Fixed url for ProductController

--- src/main/java/com/coffeeshop/controller/ProductController.java
+++ src/main/java/com/coffeeshop/controller/ProductController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/customer")
 public class ProductController {
 
     @PostMapping("/products")
