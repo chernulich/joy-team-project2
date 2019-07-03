06cf4c1c9dfd6e308f5891978778c8629d2f5f8f
sacha11@list.ru <https://github.com/chernulich>
2019-07-04 00:09:55 +0300

    It's work

---
+++ ui/customer-ui/server/product-server/product.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const jsonFile = require('jsonfile');
+const file = '.././product-server/products-response.json';
+jsonFile.readFile(file, function (err, obj) {
+    if (err) console.error(err)
+    app.post('/api/customer/products',  (req, res) => {
+        res.send(obj);
+    });
+})
+
+app.get('/', function (req, res) {
+    res.send('Hi, Joy-Team!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Node Express server listening on http://localhost:${PORT}`);
+});
