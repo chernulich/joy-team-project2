96547746681d33f4b78f17cc3bf7f56a3fde86d0
elenaU <GITHUB EMAIL>
2019-07-06 21:46:17 +0300

    add method getImagesById in Product.JS

--- ui/customer-ui/server/api/products/product.js
+++ ui/customer-ui/server/api/products/product.js
@@ -1,2 +1,12 @@
 let rest = require('restler');
+let path = require("path");
 let configuration = require('../../config/backend-config');
+
+exports.getImagesById = function (request, response) {
+
+  let dir = path.join(__dirname, '../../mock/image/rose-blue-flower-rose-blooms-67636.jpeg');
+
+  response.sendFile(dir);
+
+};
+
--- ui/customer-ui/server/app.js
+++ ui/customer-ui/server/app.js
@@ -19,7 +19,8 @@ app.get('*.*', express.static(DIST_FOLDER, {
 
 let routes = {
   api: {
-    example: require('./api/examples/example')
+    example: require('./api/examples/example'),
+    product: require("./api/products/product")
   }
 };
 
@@ -28,6 +29,7 @@ app.use(express.urlencoded({extended: false}));
 app.get('/api/examples', routes.api.example.getExampleList);
 app.get('/api/examples/:id', routes.api.example.getExampleById);
 app.post('/api/examples', routes.api.example.saveExample);
+app.get('/api/customer/products/:productId/images/:imageId',routes.api.product.getImagesById);
 
 
 // All regular routes use the Universal engine
