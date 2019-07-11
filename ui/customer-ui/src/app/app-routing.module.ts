9116d740b7078a5ca86c32d20c68cb2b9dba8007
elenaU <GITHUB EMAIL>
2019-07-11 15:45:56 +0300

    Create footer component

--- ui/customer-ui/src/app/app-routing.module.ts
+++ ui/customer-ui/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ExampleComponent} from "./example/example.component";
+import {FooterComponent} from "./footer/footer.component";
 
 const routes: Routes = [
   {path: '', component: ExampleComponent},
   {path: 'examples', component: ExampleComponent},
+  {path: 'footer', component: FooterComponent}
 ];
 
 @NgModule({
--- ui/customer-ui/src/app/app.module.ts
+++ ui/customer-ui/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { ExampleComponent } from './example/example.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleComponent
+    ExampleComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
