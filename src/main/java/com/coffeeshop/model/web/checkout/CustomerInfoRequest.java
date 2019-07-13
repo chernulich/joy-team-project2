f7f943a51691817ede5609aefa2c1fc9612564d8
sacha11@list.ru <https://github.com/chernulich>
2019-07-13 11:16:16 +0300

    Created a CheckoutRequest class and modified other Dto's

--- src/main/java/com/coffeeshop/model/web/checkout/CustomerInfoRequest.java
+++ src/main/java/com/coffeeshop/model/web/checkout/CustomerInfoRequest.java
@@ -9,15 +9,11 @@ import java.util.List;
 @Setter
 @Getter
 @Builder
-public class CustomerInfoRequest {
+class CustomerInfoRequest {
 
     private String entityName;
     private String email;
     private String phoneNumber;
-    private ContactInfoRequest contactInfoRequest;
-    private DeliveryRequest deliveryRequest;
-    private List<ProductWeightQuantityRequest> productWeightQuantityRequestList;
-    private ChargesRequest chargesRequest;
-
+    private ContactInfoRequest contacts;
 
 }
