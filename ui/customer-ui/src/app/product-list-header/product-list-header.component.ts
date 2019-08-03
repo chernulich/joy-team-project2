99cb83d28a8b7b3ebc6942348e3f5505a9e8a608
Simkin <nir.simkin@gmail.com>
2019-08-03 18:23:00 +0300

    created product-list-header and refactor footer to be more responsive

---
+++ ui/customer-ui/src/app/product-list-header/product-list-header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-product-list-header',
+  templateUrl: './product-list-header.component.html',
+  styleUrls: ['./product-list-header.component.css'],
+})
+export class ProductListHeaderComponent implements OnInit {
+  //
+  // // @ts-ignore
+  // imageF = require('../../assets/imagesSlider/header.jpg');
+  // // @ts-ignore
+  // imageS = require('../../assets/imagesSlider/beeren.jpg');
+  // // @ts-ignore
+  // imageT = require('../../assets/imagesSlider/guide-banner.jpg');
+  // // @ts-ignore
+  // imageFo = require('../../assets/imagesSlider/tropical-beach-garden-view.jpg');
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
