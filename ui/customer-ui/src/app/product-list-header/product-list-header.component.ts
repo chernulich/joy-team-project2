89c644823981a60d348a42decc9318cf09de6b3f
Simkin <nir.simkin@gmail.com>
2019-08-03 18:27:44 +0300

    fixed footer , added product-list-header.

--- ui/customer-ui/src/app/product-list-header/product-list-header.component.ts
+++ ui/customer-ui/src/app/product-list-header/product-list-header.component.ts
@@ -7,15 +7,6 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./product-list-header.component.css'],
 })
 export class ProductListHeaderComponent implements OnInit {
-  //
-  // // @ts-ignore
-  // imageF = require('../../assets/imagesSlider/header.jpg');
-  // // @ts-ignore
-  // imageS = require('../../assets/imagesSlider/beeren.jpg');
-  // // @ts-ignore
-  // imageT = require('../../assets/imagesSlider/guide-banner.jpg');
-  // // @ts-ignore
-  // imageFo = require('../../assets/imagesSlider/tropical-beach-garden-view.jpg');
 
   constructor() {
   }
