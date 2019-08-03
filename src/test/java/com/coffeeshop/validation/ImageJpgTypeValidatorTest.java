5308c1308961795d9fd1c1d4a770a0a4d335f96a
sacha11@list.ru <https://github.com/chernulich>
2019-08-03 16:40:34 +0300

    and once...

--- src/test/java/com/coffeeshop/validation/ImageJpgTypeValidatorTest.java
+++ src/test/java/com/coffeeshop/validation/ImageJpgTypeValidatorTest.java
@@ -7,8 +7,6 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Base64;
 
 import static org.junit.Assert.*;
