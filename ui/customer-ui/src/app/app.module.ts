42a1299b65313bb4d11b93ca9590f0b373bd4265
berberAlla <alysik19842807@gmail.com>
2019-08-22 12:41:04 +0300

    updated app.module.ts

--- ui/customer-ui/src/app/app.module.ts
+++ ui/customer-ui/src/app/app.module.ts
@@ -2,22 +2,20 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
-import {BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 import {AppComponent} from './app.component';
 import {ExampleComponent} from './example/example.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {HeaderComponent} from './header/header.component';
-import { OrderReceivedComponent } from './order-received/order-received.component';
-import { CheckoutComponent } from './checkout-customer/checkout.component';
+import {OrderReceivedComponent} from './order-received/order-received.component';
+import {CheckoutComponent} from './checkout-customer/checkout.component';
 import {ProductListComponent} from "./product-list/product-list.component";
 import {ProductDetailsComponent} from "./product-details/product-details.component";
-import { FooterComponent } from './footer/footer.component';
-import { ProductListHeaderComponent } from './product-list-header/product-list-header.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {FooterComponent} from './footer/footer.component';
+import {ProductListHeaderComponent} from './product-list-header/product-list-header.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {ProductSearchModule} from "./product-list/product-search/product-search.module";
-// import {ProductSearchComponent} from "./product-list/product-search/product-search.component";
-
 
 
 @NgModule({
@@ -44,4 +42,5 @@ import {ProductSearchModule} from "./product-list/product-search/product-search.
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
