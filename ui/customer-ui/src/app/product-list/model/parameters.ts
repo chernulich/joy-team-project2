21760a3b39a40f446b52cdf03cd3d0b62cf93469
Pavel Moriakov <Mpyu1992@gmail.com>
2019-09-02 10:36:48 +0300

    Change fields in ProductParameters & ProductResponse on BE and FE

--- ui/customer-ui/src/app/product-list/model/parameters.ts
+++ ui/customer-ui/src/app/product-list/model/parameters.ts
@@ -1,45 +1,27 @@
 export class Parameters {
-    private _flavour: string;
-    private _rate: number;
     private _decaf: boolean;
     private _coffeeType: string;
 
-    constructor(flavour: string, rate: number, decaf: boolean, coffeeType: string) {
-        this._flavour = flavour;
-        this._rate = rate;
-        this._decaf = decaf;
-        this._coffeeType = coffeeType;
-    }
 
-    get flavour(): string {
-        return this._flavour;
-    }
+  constructor(decaf: boolean, coffeeType: string) {
+    this._decaf = decaf;
+    this._coffeeType = coffeeType;
+  }
 
-    set flavour(value: string) {
-        this._flavour = value;
-    }
 
-    get rate(): number {
-        return this._rate;
-    }
+  get decaf(): boolean {
+    return this._decaf;
+  }
 
-    set rate(value: number) {
-        this._rate = value;
-    }
+  set decaf(value: boolean) {
+    this._decaf = value;
+  }
 
-    get decaf(): boolean {
-        return this._decaf;
-    }
+  get coffeeType(): string {
+    return this._coffeeType;
+  }
 
-    set decaf(value: boolean) {
-        this._decaf = value;
-    }
-
-    get coffeeType(): string {
-        return this._coffeeType;
-    }
-
-    set coffeeType(value: string) {
-        this._coffeeType = value;
-    }
+  set coffeeType(value: string) {
+    this._coffeeType = value;
+  }
 }
